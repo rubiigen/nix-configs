d8356044a06f3447393854bcaed3c9ed7e33f883
RionWren <rionwren@proton.me>
2023-08-16 00:27:40 +0100

    fixed a stupid mistake

--- home-manager/homes/rion/programs.nix
+++ home-manager/homes/rion/programs.nix
@@ -6,6 +6,13 @@ _: {
 	enable = true;
 	userName = "RionWren";
 	userEmail = "rionwren@proton.me";
+	extraConfig = {
+	# ssh key signed commits (why)
+	commit.gpgsign = true;
+	gpg.format = "ssh";
+	user.signingkey = "~/.ssh/id_ed25519.pub";
+	init.defaultBranch = "master";
+        };
     };
     firefox.enable = true;
     neovim.enable = true;
--- nixos/hosts/messier/configuration.nix
+++ nixos/hosts/messier/configuration.nix
@@ -93,13 +93,8 @@
     pkgs.xdg-desktop-portal-hyprland
     pkgs.libsForQt5.polkit-kde-agent
     pkgs.jetbrains-mono
+    pkgs.libsForQt5.qt5ct
   ];
-
-  fonts.fonts = with pkgs; [
-	font-awesome
-        jetbrains-mono
-  ];
-
 
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
@@ -152,20 +147,21 @@
   services.printing.enable = true;
 
   # Sound (kill me now)
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    wireplumber.enable = true;
   };
 
   # define user acc
   users.users.rion = {
     isNormalUser = true;
     description = "Rion";
-    extraGroups = ["networkmanager" "wheel" "adbusers" "audio" ];
+    extraGroups = ["networkmanager" "wheel" "adbusers"];
     openssh.authorizedKeys.keys = [
       # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
     ];
