70d3f79774bfcb64ca9358860e130a874b203f9b
RionWren <rionwren@proton.me>
2023-09-10 13:41:38 +0100

    :3

--- homes/Nebula/rion/default.nix
+++ homes/Nebula/rion/default.nix
@@ -24,10 +24,10 @@
     username = "rion";
     homeDirectory = "/home/rion";
     file.".config/hypr/hyprpaper.conf".text = ''
-      preload = ../../../wallpapers/mandelbrot_gap_magenta.png
-      wallpaper = DVI-D-1,../../../wallpapers/mandelbrot_gap_magenta.png
-      wallpaper = DP-3,../../../wallpapers/mandelbrot_gap_magenta.png
-      wallpaper = HDMI-A-1,../../../wallpapers/mandelbrot_gap_magenta.png
+      preload = ~/.config/nixos/wallpapers/mandelbrot_gap_magenta.png
+      wallpaper = DVI-D-1,~/.config/nixos/wallpapers/mandelbrot_gap_magenta.png
+      wallpaper = DP-3,~/.config/nixos/wallpapers/mandelbrot_gap_magenta.png
+      wallpaper = HDMI-A-1,~/.config/nixos/wallpapers/mandelbrot_gap_magenta.png
     '';
   };
 
