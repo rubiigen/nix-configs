548778663186bccb05a68e4d589b4c2dacc444e7
RionWren <rionwren@proton.me>
2023-09-10 12:47:40 +0100

    changed some stuff in Cygnus

--- hosts/Cygnus/configuration.nix
+++ hosts/Cygnus/configuration.nix
@@ -114,7 +114,6 @@
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
 
-  # TODO: Set your hostname
   networking.hostName = "Cygnus";
 
 
@@ -170,6 +169,11 @@
     pulse.enable = true;
   };
 
+  services.logind = {
+    extraConfig = "HandlePowerKey=suspend";
+    lidSwitch = "suspend";
+  };
+
   security.polkit.enable = true;
 
  systemd = {
@@ -200,12 +204,13 @@
 
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
-  services.openssh.settings = {
+  services.openssh = {
     enable = true;
     # Forbid root login through SSH.
-    permitRootLogin = "yes";
+    settings = {
+      PermitRootLogin = "yes";
+    };
     # Use keys only. Remove if you want to SSH using password (not recommended)
-    passwordAuthentication = false;
   };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
