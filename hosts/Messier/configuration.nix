9f704d467b0ad40eebad797f9cc87d588b7090d5
RionWren <rionwren@proton.me>
2023-09-21 23:14:12 +0100

    modified:   hosts/Messier/configuration.nix

--- hosts/Messier/configuration.nix
+++ hosts/Messier/configuration.nix
@@ -108,6 +108,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/";
+  boot.supportedFilesystems = [ "exfat" ];
   boot.kernelPackages = pkgs.linuxPackages_latest;
   
   # enable networking
