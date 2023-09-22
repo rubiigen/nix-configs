69c7d9b2d05a5a180b05d3100a48710546e70050
radiisys <rionwren@proton.me>
2023-09-22 21:48:33 +0100

    attempt 2

--- hosts/Nebula/configuration.nix
+++ hosts/Nebula/configuration.nix
@@ -106,7 +106,7 @@
   boot.loader.efi.efiSysMountPoint = "/boot/";
   boot.supportedFilesystems = [ "exfat" ];
   boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.extraModulePackages = [ "config.boot.kernelPackages.broadcom_sta" ];
+  boot.extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
 
   # enable networking
   networking.networkmanager.enable = true;
