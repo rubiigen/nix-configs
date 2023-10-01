683dacd8d4ba60f93592d2d7b6bb2ca61dc42508
radiisys <radisys@proton.me>
2023-10-01 19:55:04 +0100

    modified:   hosts/Messier/configuration.nix

--- hosts/Messier/configuration.nix
+++ hosts/Messier/configuration.nix
@@ -56,12 +56,12 @@
   environment.pathsToLink = [ "/libexec" ];
 
   dbus-sway-environment = pkgs.writeTextFile {
-    name = "dbus-sway-invironment";
+    name = "dbus-sway-environment";
     destination = "/bin/dbus-sway-environment";
     executable = true;
 
     text = ''
-      dbus-update-activation-environment --systemd WAYLAND-DISPLAY XDG-CURRENT-DESKTOP=sway
+      dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
       systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
       systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
     '';
