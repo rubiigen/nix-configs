837bd62ad804601d05475df3e825f8e4d716d890
radiisys <radisys@proton.me>
2023-10-01 11:24:50 +0100

    lets just use i3 without xfce

--- hosts/Messier/configuration.nix
+++ hosts/Messier/configuration.nix
@@ -42,14 +42,6 @@
     layout = "us";
     xkbVariant = "colemak";  
     videoDrivers = lib.mkForce [ "nvidia" ];
-    desktopManager = {
-      xterm.enable = false;
-      xfce = {
-        enable = true;
-        noDesktop = true;
-        enableXfwm = false;
-      };
-    };
     windowManager.i3 = {
       enable = true;
       extraPackages = with pkgs; [
