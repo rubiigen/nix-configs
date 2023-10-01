bc0892b68e08ed6edc1037f911a5c352880ffffe
radiisys <radisys@proton.me>
2023-10-01 19:57:12 +0100

    modified:   hosts/Messier/configuration.nix

--- hosts/Messier/configuration.nix
+++ hosts/Messier/configuration.nix
@@ -55,32 +55,6 @@
 
   environment.pathsToLink = [ "/libexec" ];
 
-  dbus-sway-environment = pkgs.writeTextFile {
-    name = "dbus-sway-environment";
-    destination = "/bin/dbus-sway-environment";
-    executable = true;
-
-    text = ''
-      dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
-      systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
-      systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
-    '';
-  };
-
-  configure-gtk = pkgs.writeTextFile {
-    name = "configure-gtk";
-    destination = "/bin/configure-gtk";
-    executable = true;
-    text = let
-      schema = pkgs.gsettings-desktop-schemas;
-      datadir = "${schema}/share/gsettings-schemas/${schema.name}";
-    in ''
-      export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
-      gnome_schema=org.gnome.desktop.interface
-      gsettings set $gnome_schema gtk-theme 'Dracula'
-    '';
-  };
-
   hardware.nvidia = {
     powerManagement.enable = true;
     powerManagement.finegrained = true;
@@ -171,6 +145,7 @@
     logitech-udev-rules
     nitrogen
     dbus-sway-environment
+    configure-gtk
     xdg-utils
     dracula-theme
     gnome3.adwaita-icon-theme
