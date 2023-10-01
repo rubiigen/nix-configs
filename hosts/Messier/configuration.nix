e6af139a91b98f3eba20e5d3c84ef68f537cd527
radiisys <radisys@proton.me>
2023-10-01 09:56:45 +0100

    oop

--- hosts/Messier/configuration.nix
+++ hosts/Messier/configuration.nix
@@ -37,11 +37,13 @@
 
   services.xserver = {
     videoDrivers = lib.mkForce [ "nvidia" ];
-    xterm.enable = false;
-    xfce = {
-      enable = true;
-      noDesktop = true;
-      enableXfwm = false;
+    desktopManager = {
+      xterm.enable = false;
+      xfce = {
+        enable = true;
+        noDesktop = true;
+        enableXfwm = false;
+      };
     };
     windowManager.i3 = {
       enable = true;
