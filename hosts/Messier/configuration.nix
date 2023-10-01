8c15c6729b8b8eabd86340495e1059264c8b6568
radiisys <radisys@proton.me>
2023-10-01 22:03:25 +0100

    thingy

--- hosts/Messier/configuration.nix
+++ hosts/Messier/configuration.nix
@@ -97,7 +97,7 @@
          powerManagement.finegrained = lib.mkForce false;
          prime.offload.enable = lib.mkForce false;
          prime.offload.enableOffloadCmd = lib.mkForce false;
-         prime.sync.enable = lib.mkForce false;
+         prime.sync.enable = lib.mkForce true;
        };
     };
   };
