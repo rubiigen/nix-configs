133c6d163c9f3b3a1c7ed9b019a26050a0c674e3
radiisys <radisys@proton.me>
2023-10-01 12:20:41 +0100

    a felony

--- homes/Messier/radisys/default.nix
+++ homes/Messier/radisys/default.nix
@@ -35,6 +35,28 @@
     '';
   };
 
+  home.pointerCursor =
+    let
+      getFrom = url: hash: name: {
+          gtk.enable = true;
+          x11.enable = true;
+          name = name;
+          size = 64;
+          package =
+            pkgs.runCommand "moveUp" {} ''
+              mkdir -p $out/share/icons
+              ln -s ${pkgs.fetchzip {
+                url = url:
+                hash = hash;
+              }} $out/share/icons/${name}
+          '';
+      };
+    in
+      getFrom
+        "https://github.com/ful1e5/fuchsia-cursor/releases/download/v2.0.0/Fuchsia-Pop.tar.gz"
+        "sha256-BvVE9qupMjw7JRqFUj1J0a4ys6kc9fOLBPx2bGaapTk="
+        "Fuchsia-Pop";
+
   wayland.windowManager.hyprland = {
     enable = true;
     systemdIntegration = true;
