198c910d577968ba2b74021561ff338909a0e904
radiisys <radisys@proton.me>
2023-10-01 11:56:17 +0100

    test

--- hosts/Messier/configuration.nix
+++ hosts/Messier/configuration.nix
@@ -158,8 +158,9 @@
   
   environment.sessionVariables = {
     GDK_DPI_SCALE = "0.5";
+    GDK_SCALE = "2";
     XCURSOR_SIZE = "32";
-    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+    #QT_AUTO_SCREEN_SCALE_FACTOR = "1";
   };
 
   xdg.portal = {
