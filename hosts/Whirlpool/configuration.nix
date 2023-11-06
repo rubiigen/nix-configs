dec768c721e10b818b60713057c4be1b45482ad1
radiisys <radisys@proton.me>
2023-11-06 20:07:00 +0000

    sway test

--- hosts/Whirlpool/configuration.nix
+++ hosts/Whirlpool/configuration.nix
@@ -5,7 +5,38 @@
   config,
   pkgs,
   ...
-}: {
+}:
+
+let
+   dbus-sway-environment = pkgs.writeTextFile {
+      name = "dbus-sway-environment";
+      destination = "/bin/dbus-sway-environment";
+      executable = true;
+
+      text = ''
+         dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
+         systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
+          systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
+      '';
+   };
+
+   configure-gtk = pkgs.writeTextFile {
+     name = "configure-gtk";
+     destination = "/bin/configure-gtk";
+     executable = true;
+     text = let
+       schema = pkgs.gsettings-desktop-schemas;
+       datadir = "${schema}/share/gsettings-schemas/${schema.name}";
+     in ''
+       export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
+       gnome_schema=org.gnome.desktop.interface
+       gsettings set $gnome_schema gtk-theme 'Dracula'
+     '';
+   };
+
+ in
+
+ {
   imports = [
     ./hardware-configuration.nix
   ];
@@ -70,9 +101,28 @@
     nm-applet.enable = true;
     adb.enable = true;
     dconf.enable = true;
+    sway = {
+      enable = true;
+      wrapperFeatures.gtk = true;
+    };	
     };
 
   environment.systemPackages = with pkgs; [
+    dbus-sway-environment
+    configure-gtk
+    wayland
+    xdg-utils
+    glib
+    dracula-theme
+    gnome3.adwaita-icon-theme
+    swaylock
+    swayidle
+    grim
+    slurp
+    wl-clipboard
+    bemenu
+    mako
+    wdisplays
     autorandr
     openrgb-with-all-plugins
     cinnamon.nemo
@@ -117,6 +167,12 @@
   services.logind = {
     extraConfig = "HandlePowerKey=suspend";
   };
+
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  }; 
 
   console.useXkbConfig = true;
 
