963a4edc594c8c621cfc81f4c7b0a494f3489768
rubiigen <alyxwren@proton.me>
2024-01-25 01:26:05 +0000

    wa

--- hosts/Hyperion/configuration.nix
+++ hosts/Hyperion/configuration.nix
@@ -333,12 +333,13 @@ in
 
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
+
+  services.openssh.enable = false;
   services.openssh.settings = {
-    enable = false;
     # Forbid root login through SSH.
-    permitRootLogin = "yes";
+    PermitRootLogin = "no";
     # Use keys only. Remove if you want to SSH using password (not recommended)
-    passwordAuthentication = false;
+    PasswordAuthentication = false;
   };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
