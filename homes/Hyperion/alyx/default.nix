d4288ad3793f33daf2a38ce7f63d8d3ff63a181c
rubiigen <alyxwren@proton.me>
2024-01-26 21:16:16 +0000

    h

--- homes/Hyperion/alyx/default.nix
+++ homes/Hyperion/alyx/default.nix
@@ -17,7 +17,7 @@
     ../../common/arrpc.nix
     ../../common/packages.nix
     ../../common/programs.nix
-    ../../../.gitignore/gita.nix
+    ../../common/gita.nix
   ];
 
   # TODO: Set your username
---
+++ homes/common/gitm.nix
@@ -0,0 +1,17 @@
+_: {
+  # enable programs
+  programs = {
+    git = {
+	enable = true;
+	userName = "beanigen";
+	userEmail = "bigyeetusthefirst@protonmail.com";
+	extraConfig = {
+		commit.gpgsign = true;
+		gpg.format = "ssh";
+		user.signingkey = "~/.ssh/id_ed25519.pub";
+		init.defaultBranch = "master";
+        };
+    };
+    # ... add more programs as you see fit
+  };
+}
