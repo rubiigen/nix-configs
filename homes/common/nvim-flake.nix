7c92468bfd1359e54991ce30d0537b88f58f2be0
beanigen <beanigit@protonmail.com>
2024-02-09 11:13:18 +0800

    getting the hang of nvim-flake config, enjoy and sorry for the mess in there lmfao

--- homes/common/nvim-flake.nix
+++ homes/common/nvim-flake.nix
@@ -1,23 +1,52 @@
 {inputs, ...}: { 
-
+ # i apologise for how much of a fucking mess this file is lmfao -maya
   imports = [ inputs.neovim-flake.homeManagerModules.default ];
   # enable programs
   programs = {
     neovim-flake = {
       enable = true;
       settings = {
-        vim.languages = {
-	        nix.enable = true;
-          nix.lsp.enable = true;
-	        ts.enable = true;
-	        python.enable = true;
-	        html.enable = true;
-          enableFormat = true;
-          enableLSP = true;
-          enableTreesitter = true;
-        };
-        vim.lsp = {
-          enable = true;
+        vim = {
+
+
+          theme = {
+            enable = true;
+            name = "catppuccin";
+            style = "mocha";
+          };
+
+          languages = {
+	          nix.enable = true;
+            nix.lsp.enable = true;
+	          ts.enable = true;
+	          python.enable = true;
+	          html.enable = true;
+            enableFormat = true;
+            enableLSP = true;
+            enableTreesitter = true;
+          };
+
+          autopairs.enable = true;
+
+          statusline.lualine = {
+            enable = true;
+          };
+          dashboard.alpha = {
+            enable = true;
+            # idk need to configure this -maya
+          };
+
+          ui = {
+            borders = {
+              enable = true;
+            };
+            breadcrumbs.enable = true;
+          };
+
+          visuals.smoothScroll.enable = true;
+          lsp = {
+            enable = true;
+          };
         };
       };
     };
