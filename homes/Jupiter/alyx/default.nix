f1e3d5758617aadd9ef74fd1cb4d3d418ca40164
rubiigen <alyxwren@proton.me>
2024-04-12 04:03:12 +0100

    Add the rest of Jupiter's config (MacPro5,1)

---
+++ homes/Jupiter/alyx/default.nix
@@ -0,0 +1,72 @@
+{
+  pkgs,
+  lib,
+  inputs,
+  ...
+}: {
+  imports = [
+    ../../common/arrpc.nix
+    ../../common/packages.nix # home.packages and similar stuff
+    ../../common/programs.nix # programs.<programName>.enable
+    ../../common/gita.nix
+    ../../common/nvim-flake.nix
+    ../../common/ags
+    ../../common/files.nix
+  ];
+
+  home = {
+    username = "alyx";
+    homeDirectory = "/home/alyx";
+    pointerCursor = {
+      gtk.enable = true;
+      x11.enable = true;
+      package = pkgs.catppuccin-cursors.mochaMauve;
+      name = "Catppuccin-Mocha-Mauve-Cursors";
+      size = 40;
+    };
+  };
+
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Catppuccin-Mocha-Standard-Mauve-Dark";
+      package = pkgs.catppuccin-gtk.override {
+        accents = ["mauve"];
+        variant = "mocha";
+      };
+    };
+
+    cursorTheme = {
+      name = "Catppuccin-Mocha-Mauve-Cursors";
+      package = pkgs.catppuccin-cursors.mochaMauve;
+      size = 40;
+    };
+  };
+  
+  qt.enable = true;
+  qt.platformTheme = "gtk";
+
+  systemd.user.targets.tray = {
+    Unit = {
+      Description = "Home Manager System Tray";
+      Requires = ["graphical-session-pre.target"];
+    };
+  };
+
+  wayland.windowManager.hyprland = {
+    enable = true;
+    systemd.enable = true;
+    settings = import ./hyprland.nix;
+  };
+
+  services.udiskie.enable = true;
+
+  # let HM manage itself when in standalone mode
+  programs.home-manager.enable = true;
+
+  # Nicely reload system(d) units when changing configs
+  systemd.user.startServices = lib.mkDefault "sd-switch";
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "24.05";
+}
---
+++ hosts/Jupiter/configuration.nix
@@ -0,0 +1,183 @@
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ./hardware-configuration.nix
+    ../common.nix
+  ];
+
+  hardware.opengl = {
+    driSupport32Bit = true;
+    extraPackages = with pkgs; [
+      amdvlk
+      vulkan-validation-layers
+      libvdpau-va-gl
+      vaapiVdpau
+      mesa
+      rocmPackages.clr.icd
+    ];
+    extraPackages32 = with pkgs; [
+      driversi686Linux.amdvlk
+      driversi686Linux.mesa
+    ];
+  };
+
+
+  services.xserver = {
+    enable = true;
+    videoDrivers = ["amdgpu"];
+  };
+
+  environment.pathsToLink = [ "/libexec" ];
+
+  hardware.enableAllFirmware = true;
+
+  nixpkgs = {
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+    };
+  };
+
+  nix = {
+    registry = lib.mapAttrs (_: value: {flake = value;}) inputs;
+
+    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
+
+    settings = {
+      experimental-features = "nix-command flakes";
+      auto-optimise-store = true;
+    };
+  };
+
+  programs = {
+    adb.enable = true;
+    virt-manager.enable = true;
+    nix-ld.enable = true;
+    nix-ld.libraries = with pkgs; [
+      pkgs.libusb1
+      pkgs.cargo
+      pkgs.rustc
+      pkgs.pkg-config
+      pkgs.cacert
+    ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    alvr
+    ddcutil
+    i2c-tools
+    (pkgs.python3.withPackages (ps: with ps; [tkinter]))
+    sidequest
+    tpm2-tss
+  ];
+
+  environment.variables = {
+    ROC_ENABLE_PRE_VEGA = "1";
+  };
+
+  hardware.i2c.enable = true;
+  hardware.keyboard.qmk.enable = true;
+
+  services.ddccontrol.enable = true;
+  services.logind = {
+    extraConfig = "HandlePowerKey=suspend";
+  };
+
+  console.useXkbConfig = true;
+
+  networking.hostName = "Jupiter";
+
+  virtualisation.libvirtd = {
+    enable = true;
+    extraConfig = ''
+      user="alyx"
+    '';
+    qemu.ovmf.enable = true;
+    qemu.package = pkgs.qemu_kvm;
+    qemu.runAsRoot = true;
+    qemu.swtpm.enable = true;
+  };
+
+  virtualisation.spiceUSBRedirection.enable = true;
+
+  boot.bootspec.enable = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.initrd.luks.devices = {
+    crypted = {
+      device = "/dev/disk/by-partuuid/fcf37e6e-4b0d-407d-856a-d68d225f8c76";
+      header = "/dev/disk/by-partuuid/88a42cb8-c6c5-411b-9738-34481dd44d3b";
+      allowDiscards = true;
+      preLVM = true;
+    };
+  };
+
+  boot.loader.efi.efiSysMountPoint = "/boot/";
+  boot.supportedFilesystems = ["exfat" "xfs" "ntfs"];
+  boot.kernelParams = ["preempt=voluntary" "module_blacklist=nouveau" "intel_iommu=on" "iommu=pt" "pcie_acs_override=downstream,multifunction"];
+  boot.initrd.kernelModules = ["vfio_pci" "vfio_iommu_type1" "vfio" "kvm-intel"];
+  boot.kernelModules = ["vfio_virqfd" "vhost-net"];
+  boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
+  boot.blacklistedKernelModules = ["nouveau"];
+
+  # enable networking
+  networking.networkmanager.wifi.backend = "iwd";
+
+  # Set a time zone
+  time.timeZone = "Europe/London";
+
+  i18n.defaultLocale = "it_IT.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_GB.UTF-8";
+    LC_IDENTIFICATION = "en_GB.UTF-8";
+    LC_MEASUREMENT = "en_GB.UTF-8";
+    LC_MONETARY = "en_GB.UTF-8";
+    LC_NAME = "en_GB.UTF-8";
+    LC_NUMERIC = "en_GB.UTF-8";
+    LC_PAPER = "en_GB.UTF-8";
+    LC_TELEPHONE = "en_GB.UTF-8";
+    LC_TIME = "en_GB.UTF-8";
+  };
+
+  users.users.alyx = {
+    isNormalUser = true;
+    description = "alyx";
+    extraGroups = ["networkmanager" "wheel" "adbusers" "libvirtd" "kvm"];
+    openssh.authorizedKeys.keys = [
+      # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
+    ];
+  };
+
+  services.pipewire = {
+    extraConfig = {
+      pipewire = {
+        "10-clock-rate" = {
+          "context.properties" = {
+            "default.clock.rate" = 96000;
+          };
+        };
+      };
+      pipewire-pulse = {
+        "11-pulse-clock-rate" = {
+          "pulse.properties" = {
+            "pulse.default.req" = "128/96000";
+          };
+        };
+      };
+    };
+  };
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "yes";
+      PasswordAuthentication = true;
+    };
+  };
+}
